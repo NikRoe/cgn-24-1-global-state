d3b1e80ab078030fa0fb85f2a3232512892ef69d
Niklas Röttger <niklas.roettger@outlook.de>
2024-02-15 10:42:02 +0100

    add demo code

--- components/CounterList.js
+++ components/CounterList.js
@@ -10,15 +10,23 @@ const List = styled.ul`
   gap: 1rem;
 `;
 
-export default function CounterList() {
+export default function CounterList({ animals, handleAdd, handleSubtract }) {
   return (
     <>
       <h2>Counters</h2>
       <List>
-        <Counter animalName={"Cats"} />
-        <Counter animalName={"Dogs"} />
-        <Counter animalName={"Sheep"} />
-        <Counter animalName={"Dragons"} />
+        {animals.map((animal) => {
+          return (
+            <Counter
+              key={animal.id}
+              animalName={animal.name}
+              handleAdd={handleAdd}
+              handleSubtract={handleSubtract}
+              id={animal.id}
+              count={animal.count}
+            />
+          );
+        })}
       </List>
     </>
   );
